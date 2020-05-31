aba7a610fe828298e265406cfb6fbdbcec4a08bb
BenjaminPichon <55100574+BenjaminPichon@users.noreply.github.com>
2020-05-31 14:07:14 +0200

    ADD entity
    I add the task and employee entity

---
+++ test_GTP/src/Entity/Task.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TaskRepository::class)
+ */
+class Task
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Libelle;
+
+    /**
+     * @ORM\Column(type="time")
+     */
+    private $Starttime;
+
+    /**
+     * @ORM\Column(type="time")
+     */
+    private $Endtime;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Employee::class, inversedBy="tasks")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $Employee;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->Libelle;
+    }
+
+    public function setLibelle(string $Libelle): self
+    {
+        $this->Libelle = $Libelle;
+
+        return $this;
+    }
+
+    public function getStarttime(): ?\DateTimeInterface
+    {
+        return $this->Starttime;
+    }
+
+    public function setStarttime(\DateTimeInterface $Starttime): self
+    {
+        $this->Starttime = $Starttime;
+
+        return $this;
+    }
+
+    public function getEndtime(): ?\DateTimeInterface
+    {
+        return $this->Endtime;
+    }
+
+    public function setEndtime(\DateTimeInterface $Endtime): self
+    {
+        $this->Endtime = $Endtime;
+
+        return $this;
+    }
+
+    public function getEmployee(): ?Employee
+    {
+        return $this->Employee;
+    }
+
+    public function setEmployee(?Employee $Employee): self
+    {
+        $this->Employee = $Employee;
+
+        return $this;
+    }
+}
